1f87e9d2f8bfb773454cfee3a54a8aa931e22800
paulacarolina203040-max <paulacarolina203040@gmail.com>
2026-09-07 15:14:44 -0300

    Split 2

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -4,17 +4,14 @@ from app.split_integer import split_integer
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
     assert sum(split_integer(17, 4)) == 17
     assert sum(split_integer(8, 1)) == 8
-    assert sum(split_integer(3, 5)) == 3
 
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
     assert split_integer(6, 2) == [3,3]
-    assert split_integer(10, 5) == [2, 2, 2, 2, 2]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
     assert split_integer(8,1) == [8]
-    assert split_integer(5,1) == [5]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
@@ -24,12 +21,8 @@ def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    result_two = split_integer(2, 4)
-    assert result_two == [0, 0, 1, 1]
-    assert result_two == sorted(result_two)
-    assert len(result_two) == 4
-
-    result_three = split_integer(3, 5)
-    assert result_three == [0, 0, 0, 1, 1]
-    assert len(result_three) == 5
-    assert sum(result_three) == 3
+    result = split_integer(2, 4)
+    assert result == [0, 0, 1, 1]
+    assert len(result) == 4
+    assert sum(result) == 2
+    assert max(result) - min(result) <= 1
